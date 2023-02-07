2bd2fb9a190276179e5cbd660335fe1c0a32b007
ersonp <ersonpereiracr7@gmail.com>
2023-02-07 18:34:50 +0530

    Update navbar to use Link instead of a tag

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Switcher from './Switcher';
 
 export default function Navbar() {
   return (
     <nav className="container w-full max-w-7xl mx-auto pt-4 pl-4 pr-4 overflow-hidden flex">
-      <a href="/" className="flex items-center mr-4">
+      <Link to="/" className="flex items-center mr-4">
         <span className="font-bold text-xl tracking-tight fill-current text-primary-color dark:text-secondary-color bg-secondary-color dark:bg-primary-color pr-1 pl-1">EP</span>
-      </a>
+      </Link>
       <div className="flex-grow flex items-center w-auto">
         <div className="text-sm flex-grow">
-          <a href="/about" className="inline-block hover:underline decoration-4 mr-4">
+          <Link to="/about" className="inline-block hover:underline decoration-4 mr-4">
             about
-          </a>
-          <a href="/contact" className="inline-block hover:underline decoration-4 mr-4">
+          </Link>
+          <Link to="/contact" className="inline-block hover:underline decoration-4 mr-4">
             contact
-          </a>
+          </Link>
         </div>
       </div>
       <div className="flex items-center pt-7">
