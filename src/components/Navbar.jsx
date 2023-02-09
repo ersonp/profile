a1f755ae57be8f37f0ea3063d971b1663073683d
ersonp <ersonpereiracr7@gmail.com>
2023-02-09 20:13:52 +0530

    Update favicon
    Update the favicon and logo to .dev.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ export default function Navbar() {
   return (
     <nav className="container w-full max-w-7xl mx-auto pt-4 pl-4 pr-4 overflow-hidden flex">
       <Link to="/" className="flex items-center mr-4">
-        <span className="font-bold text-xl tracking-tight fill-current text-primary-color dark:text-secondary-color bg-secondary-color dark:bg-primary-color pr-1 pl-1">EP</span>
+        <span className="font-bold text-xl tracking-tight fill-current text-primary-color dark:text-secondary-color bg-secondary-color dark:bg-primary-color pr-1 pl-1">.dev</span>
       </Link>
       <div className="flex-grow flex items-center w-auto">
         <div className="text-sm flex-grow">
