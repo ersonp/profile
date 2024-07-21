9d4365ce78bd857ec0ec92d6f0140d575e8905b7
ersonp <ersonpereiracr7@gmail.com>
2024-07-22 00:52:19 +0530

    chore: Update resume PDF

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -50,7 +50,7 @@ const experience = {
     {
       title: 'Go Developer',
       content: '@ Skycoin',
-      date: '2021-2023',
+      date: '2021-now',
     },
     {
       title: 'Software Developer',
@@ -123,7 +123,7 @@ export default function About() {
               <button type="button" className="bg-secondary-color dark:bg-primary-color text-primary-color dark:text-secondary-color py-0.5 px-4 rounded-full items-center">
                 <div className="flex flex-grow">
                   CV&nbsp;
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="fill-current mt-1 fill-primary-color dark:fill-secondary-color" viewBox="0 0 16 16">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="mt-1 fill-primary-color dark:fill-secondary-color" viewBox="0 0 16 16">
                     <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z" />
                     <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z" />
                   </svg>
